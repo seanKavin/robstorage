e3271094a2e8b5fa21ef00bebdbae9b6347d7579
SeanIhp <dotcom@sina.com>
2020-03-29 23:25:03 +0800

    ee

--- routes/index.js
+++ routes/index.js
@@ -1,47 +1,54 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var user = require('../models/user').user;
+// var user = require('../models/user').user;
+
 mongoose.connect('mongodb://localhost/robmit');
 
+/** * 连接异常 */ 
 mongoose.connection.on('error', () => {
   console.log('Mongoose connection error')
 });
+
+/** * 连接成功 */ 
 mongoose.connection.on('open', () => {
   console.log('Mongoose connection success')
-});
- 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
- 
-/*login*/
-router.get('/login', function(req, res) {
-    res.render('login', { title: 'login' });
-});
- 
-/*logout*/
-router.get('/logout', function(req, res) {
-      res.render('logout', { title: 'logout' });
-});
- 
-/*hompage*/
-router.post('/homepage', function(req, res) {
-    var query_doc = {userid: req.body.userid, password: req.body.password};
-    (function(){
-        user.count(query_doc, function(err, doc){
-            if(doc == 1){
-                console.log(query_doc.userid + ": login success in " + new Date());
-                res.render('homepage', { title: 'homepage' });
-            }else{
-                console.log(query_doc.userid + ": login failed in " + new Date());
-                res.redirect('/');
-            }
-        });
-    })(query_doc);
-});
+}); 
 
-
+/** * 连接断开 */ 
+mongoose.connection.on('disconnected', function () { 
+    console.log('Mongoose connection disconnected'); 
+});
+ 
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+ 
+// /*login*/
+// router.get('/login', function(req, res) {
+//     res.render('login', { title: 'login' });
+// });
+ 
+// /*logout*/
+// router.get('/logout', function(req, res) {
+//       res.render('logout', { title: 'logout' });
+// });
+ 
+// /*hompage*/
+// router.post('/homepage', function(req, res) {
+//     var query_doc = {userid: req.body.userid, password: req.body.password};
+//     (function(){
+//         user.count(query_doc, function(err, doc){
+//             if(doc == 1){
+//                 console.log(query_doc.userid + ": login success in " + new Date());
+//                 res.render('homepage', { title: 'homepage' });
+//             }else{
+//                 console.log(query_doc.userid + ": login failed in " + new Date());
+//                 res.redirect('/');
+//             }
+//         });
+//     })(query_doc);
+// });
 
 module.exports = router;
--- routes/profile/index.js
+++ routes/profile/index.js
@@ -1,16 +1,45 @@
 
 var express = require('express');
+var mongoose = require("mongoose");
+var moment = require('moment');
 var router = express.Router();
+var Account = require('../../models/account');
 
 router.get('/', function(req, res, next) {
   res.send("Profile datas...");
 });
  
-/*login*/
+/* login */
 router.post('/login', function(req, res) {
     res.send({
         "action": "profileLogin"
-    })
+    });
+});
+
+/* regist */
+router.post('/regist', function(req, res) {
+    console.log("-----Account: ", Account)
+    console.log("-----Accountqwq: ", req.body)
+    var accountVo = new mongoose.models.Account({
+        username: req.body.username,
+        password: req.body.password,
+        type: req.body.type,
+        create_time: moment(new Date()).format('YYYY-MM-DD HH:mm:ss.ms')
+    });
+    // let accountVo = {
+    //     username: req.body.username,
+    //     password: req.body.password,
+    //     type: req.body.type,
+    //     create_time: moment(new Date()).format('YYYY-MM-DD HH:mm:ss.ms')
+    // };
+    accountVo.save(function(err, res){
+        if(err){
+            console.log("Error:" + err);
+        }else{
+            console.log("res:  ", res);
+            res.send(`new account [name:${account.name}] saved at ${account.create_time}!`);
+        }
+    });
 });
 
 module.exports = router;
